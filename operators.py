43ac380252fd50fc5d1310ea9be2b2c03ff91751
Israel Ozioma Idahosa <israelidahosa24@gmail.com>
2024-09-08 22:53:10 +0100

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,83 @@
+'''print(4 + 4)
+
+print(19 - 24)
+
+print(63 * 11)
+
+print(693 / 63)
+
+print(2 ** 10)
+
+print(25 // 5) '''
+
+#arithemetic operators
+mathematics = 59
+economics = 90
+
+print(mathematics + economics)
+print(mathematics - economics)
+print(mathematics * economics)
+print(economics / mathematics)
+
+# assignment operators is used to assign values to variables
+messi = 10
+print(messi)
+messi = 10
+messi += 15
+print(messi)
+
+ronaldo = 7 
+ronaldo -= 7
+print(ronaldo)
+
+#Comparison operators: This is used to compare two values together 
+players_no = 9
+standings = 5
+print(players_no)
+print(standings)
+print(players_no == standings)
+
+seasons = 167
+episodes = 1069
+print(seasons == episodes)
+
+minutes = 1567
+seconds = 1567
+print(minutes == seconds)
+
+#not equal comparison (!=)
+products = 4786
+services = 3490
+print(products != services)
+
+#>=/<= this is used to return true or false 
+terms = 78934
+session = 23456
+print(terms >= session)
+print(session >= terms)
+print(terms <= session)
+print(session <= terms)
+
+#logical operators: These are used to combine conditional statements
+serial_number = 98
+print(serial_number > 90 and serial_number < 100)
+print(serial_number > 90 or serial_number < 100)
+print(serial_number > 90 is serial_number < 100)
+
+#python identity operators: these are used to compare objects, not if they're equal, but if they are actually the same objects with the same memory locaion.
+name_1 = ['israel', 'ehis']
+last_name = ['israel', 'ehis']
+surname = name_1
+print(name_1 is surname)
+print(name_1 is last_name)
+print(name_1 == last_name)
+
+#is not 
+print(name_1 is not surname)
+print(name_1 is not last_name)
+print(name_1 != last_name)
+
+
+
+
+
